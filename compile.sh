eec6414c3b5e2e0221942fbbf96070c8f9a7eaa3
Pedro Pires <pedro.pires@dito.com.br>
2012-10-27 21:58:38 -0200

    checkpoint

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-6-sun
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export ANT_HOME=$DIR/tools/apache-ant-1.8.4
+echo "Compilando Freemind..."
+cd $SRC/freemind/freemind && ${ANT_HOME}/bin/ant
+exit 0
+
