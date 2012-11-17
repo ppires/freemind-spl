01d9744d00604f100e8d278a16daa37732fdde39
Pedro Pires <pedro.pires@dito.com.br>
2012-11-17 18:26:00 -0200

    removing Poo...

--- freemind/freemind/freemind/main/FreeMind.java
+++ freemind/freemind/freemind/main/FreeMind.java
@@ -705,20 +705,6 @@ public class FreeMind extends JFrame implements FreeMindMain {
 	}
 
 	public static void main(final String[] args, Properties pDefaultPreferences, Properties pUserPreferences, File pAutoPropertiesFile) {
-		
-		
-		
-		
-        //#if defined(poo)
-        System.out.println("Poo is ${poo}");
-        //#else
-        System.out.println("Poo is not defined");
-        //#endif
-        
-        
-        
-        
-        
 		final FreeMind frame = new FreeMind(pDefaultPreferences, pUserPreferences, pAutoPropertiesFile);
 		IFreeMindSplash splash = null;
 		final FeedBack feedBack;
