cc3c3e4491dee0ffc2b20ecb091f82cd3012e99f
Pedro Pires <pedro.pires@dito.com.br>
2012-11-25 02:10:18 -0200

    blink feature extraction

--- freemind/freemind/accessories/plugins/BlinkingNodeHook.java
+++ freemind/freemind/accessories/plugins/BlinkingNodeHook.java
@@ -1,3 +1,5 @@
+//#if defined(BLINK)
+//@#$LPS-BLINK:GranularityType:Class
 /*FreeMind - A Program for creating and viewing Mindmaps
 *Copyright (C) 2000-2006 Joerg Mueller, Daniel Polansky, Christian Foltin, Dimitri Polivaev and others.
 *
@@ -113,3 +115,4 @@ public class BlinkingNodeHook extends PermanentMindMapNodeHookAdapter {
 	}
 
 }
+//#endif
